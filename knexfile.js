7eb43bc547985f996897beef5402505af21c12ca
kamos1 <keji.amos@gmail.com>
2017-07-24 10:06:30 -0600

    setup knex

---
+++ knexfile.js
@@ -0,0 +1,40 @@
+
+module.exports = {
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/trivial_test' || process.env.DATABASE_URL,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
+  },
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/trivial',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  }
+
+};
